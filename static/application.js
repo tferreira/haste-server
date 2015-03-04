b88de888bb2f4f6b84f3f923bcb7d76e1f115c08
Thomas Ferreira <thomas_ferreira@yahoo.fr>
2015-03-04 17:54:04 +0100

    Frontend: Add tags box on save modal

--- static/application.js
+++ static/application.js
@@ -100,6 +100,7 @@ var haste = function(appName, options) {
   this.configureButtons();
   this.configureSearch();
   this.configureModals();
+  this.configureSaveTags();
   // If del is disabled, hide the button
   if (!options.del) {
     $('#box2 .del').hide();
@@ -162,6 +163,73 @@ haste.prototype.configureModals = function() {
           }
       }
   });
+}
+
+haste.prototype.configureSaveTags = function() {
+    var _this = this;
+
+    var availableTags = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"
+    ];
+    function split( val ) {
+        return val.split( /,\s*/ );
+    }
+    function extractLast( term ) {
+        return split( term ).pop();
+    }
+    $( "#save_tags" )
+        // don't navigate away from the field on tab when selecting an item
+        .bind( "keydown", function( event ) {
+            if ( event.keyCode === $.ui.keyCode.TAB &&
+                $( this ).autocomplete( "instance" ).menu.active ) {
+                event.preventDefault();
+            }
+        })
+        .autocomplete({
+        minLength: 0,
+        source: function( request, response ) {
+            // delegate back to autocomplete, but extract the last term
+            response( $.ui.autocomplete.filter(
+            availableTags, extractLast( request.term ) ) );
+        },
+        focus: function() {
+            // prevent value inserted on focus
+            return false;
+        },
+        select: function( event, ui ) {
+            var terms = split( this.value );
+            // remove the current input
+            terms.pop();
+            // add the selected item
+            terms.push( ui.item.value );
+            // add placeholder to get the comma-and-space at the end
+            terms.push( "" );
+            this.value = terms.join( ", " );
+            return false;
+        }
+    });
+
 }
 
 // Set up the search field
